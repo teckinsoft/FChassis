f244b1e92744479e6f51851f435b599c40e0a79b
partha <149563725+parthateckinsoft@users.noreply.github.com>
2025-03-06 19:15:48 +0530

    FCH-1 Version no Display

---
+++ FChassis/AboutWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace FChassis {
+   /// <summary>
+   /// Interaction logic for About.xaml
+   /// </summary>
+   public partial class AboutWindow : Window {
+      public AboutWindow () {
+         InitializeComponent ();
+         DataContext = this;
+      }
+      public string Version { get; } = "55.4";
+      void OnAboutCloseClick (object sender, RoutedEventArgs e) => this.Close (); 
+   }
+}
