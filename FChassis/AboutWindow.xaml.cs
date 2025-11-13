53c1b92b2ff08b6ff9c85caabd9da54a94e3baf8
partha <149563725+parthateckinsoft@users.noreply.github.com>
2025-11-13 18:16:43 +0530

    FCH-36 Version change release 1.0.8 , Debug 73, Test Release 73

--- FChassis/AboutWindow.xaml.cs
+++ FChassis/AboutWindow.xaml.cs
@@ -11,11 +11,11 @@ namespace FChassis {
       }
 
 #if DEBUG
-      public string Version { get; } = "Debug 72";
+      public string Version { get; } = "Debug 73";
 #elif TESTRELEASE
-      public string Version { get; } = "Test Release 72";
+      public string Version { get; } = "Test Release 73";
 #else 
-      public string Version { get; } = "1.0.7";
+      public string Version { get; } = "1.0.8";
 #endif
       void OnAboutCloseClick (object sender, RoutedEventArgs e) => this.Close ();
    }
