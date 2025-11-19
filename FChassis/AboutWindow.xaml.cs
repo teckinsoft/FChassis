1195233d239b0ef6262226a3e324f1edc324e3fe
partha <149563725+parthateckinsoft@users.noreply.github.com>
2025-11-19 21:14:21 +0530

    FCH-14 Release of 1.0.8 (release) Debug 74 and Test Release 74

--- FChassis/AboutWindow.xaml.cs
+++ FChassis/AboutWindow.xaml.cs
@@ -11,11 +11,11 @@ namespace FChassis {
       }
 
 #if DEBUG
-      public string Version { get; } = "Debug 73";
+      public string Version { get; } = "Debug 74";
 #elif TESTRELEASE
-      public string Version { get; } = "Test Release 73";
+      public string Version { get; } = "Test Release 74";
 #else 
-      public string Version { get; } = "1.0.8";
+      public string Version { get; } = "1.0.9";
 #endif
       void OnAboutCloseClick (object sender, RoutedEventArgs e) => this.Close ();
    }
