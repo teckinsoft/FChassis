dc92792bcbd1d7c7fcbed45264dec004d3d8c7b5
partha <149563725+parthateckinsoft@users.noreply.github.com>
2025-12-01 14:16:53 +0530

    FCH-36 New release 1.0.12, Debug 77 and Test Release 77

--- FChassis/AboutWindow.xaml.cs
+++ FChassis/AboutWindow.xaml.cs
@@ -11,11 +11,11 @@ namespace FChassis {
       }
 
 #if DEBUG
-      public string Version { get; } = "Debug 75";
+      public string Version { get; } = "Debug 77";
 #elif TESTRELEASE
-      public string Version { get; } = "Test Release 75";
+      public string Version { get; } = "Test Release 77";
 #else 
-      public string Version { get; } = "1.0.10";
+      public string Version { get; } = "1.0.12";
 #endif
       void OnAboutCloseClick (object sender, RoutedEventArgs e) => this.Close ();
    }
