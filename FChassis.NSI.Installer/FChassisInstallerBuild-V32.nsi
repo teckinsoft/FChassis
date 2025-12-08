10871dc252e75bdf1960a09eef0a2fbdcebf62d4
partha <149563725+parthateckinsoft@users.noreply.github.com>
2025-12-08 22:38:59 +0530

    FCH-36 New release 1.0.15, Debug 80 and Test Release 80

--- FChassis.NSI.Installer/FChassisInstallerBuild-V32.nsi
+++ FChassis.NSI.Installer/FChassisInstallerBuild-V32.nsi
@@ -10,7 +10,7 @@
 ; General Settings
 ; -----------------------------------------------------------------------------
 !define APPNAME "FChassis"
-!define VERSION "1.0.14"
+!define VERSION "1.0.15"
 !define COMPANY "Teckinsoft Neuronics Pvt. Ltd."
 !define INSTALLDIR "C:\FChassis"
 !define FluxSDKBin "C:\FluxSDK\Bin"
--- FChassis/AboutWindow.xaml.cs
+++ FChassis/AboutWindow.xaml.cs
@@ -11,11 +11,11 @@ namespace FChassis {
       }
 
 #if DEBUG
-      public string Version { get; } = "Debug 79";
+      public string Version { get; } = "Debug 80";
 #elif TESTRELEASE
-      public string Version { get; } = "Test Release 79";
+      public string Version { get; } = "Test Release 80";
 #else 
-      public string Version { get; } = "1.0.14";
+      public string Version { get; } = "1.0.15";
 #endif
       void OnAboutCloseClick (object sender, RoutedEventArgs e) => this.Close ();
    }
